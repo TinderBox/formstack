fb78ee7f407dead5879aca492ca92574714cd38c
Joe Heth <joeheth@gmail.com>
2014-12-01 15:25:16 -0500

    Handle error when getting submission data.

--- lib/formstack/submission.rb
+++ lib/formstack/submission.rb
@@ -2,7 +2,7 @@ module FormStack
 	class Submission < HashAttributeClass
 
 		def value_of(id)
-			data = self["data"]
+			data = self["data"] || []
 			data.each do |d|
 				if d["field"].to_s == id.to_s
 					return d["value"]
